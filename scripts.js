e21e94bb4f8952fc5b7c832d29927a30891405c4
jessica eckert <jessicaconter520@gmail.com>
2024-07-29 23:03:34 -0300

    projeto inicial

---
+++ scripts.js
@@ -0,0 +1,33 @@
+function encrypt(text) {
+    return text.replace(/e/g, "enter")
+               .replace(/i/g, "imes")
+               .replace(/a/g, "ai")
+               .replace(/o/g, "ober")
+               .replace(/u/g, "ufat");
+}
+
+function decrypt(text) {
+    return text.replace(/enter/g, "e")
+               .replace(/imes/g, "i")
+               .replace(/ai/g, "a")
+               .replace(/ober/g, "o")
+               .replace(/ufat/g, "u");
+}
+
+function performEncryption() {
+    const inputText = document.getElementById("inputText").value;
+    const outputText = encrypt(inputText);
+    document.getElementById("outputText").value = outputText;
+}
+
+function performDecryption() {
+    const inputText = document.getElementById("inputText").value;
+    const outputText = decrypt(inputText);
+    document.getElementById("outputText").value = outputText;
+}
+
+function copyToClipboard() {
+    const outputText = document.getElementById("outputText");
+    outputText.select();
+    document.execCommand("copy");
+}
